d89a8d466d003b10997e4d9f4dd93c5e02ae1c76
Oliver <o.panzer@yahoo.com>
2021-07-21 17:19:11 +0200

    handle sum exceptions

--- BenfordSet/Program.cs
+++ BenfordSet/Program.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Text;
-///using iTextSharp.text.pdf;
 using System.Text.RegularExpressions;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.Content;
+using System.Collections.Generic;
+using System.Linq;
 
 
 namespace BenfordSet
@@ -12,31 +15,44 @@ namespace BenfordSet
         {
             public string Filename { get; private set; }
             public string Foldername { get; private set; }
+            public string PdfContent { get; set; }
 
-            public string PdfContent { get; private set; }
-
-            public string Numbers { get; set; }
-
-
-            public void ReadPdf() ///(string Foldername, string Filename)
+            public void ReadPdf() 
             {
                 string content = System.IO.File.ReadAllText(Foldername + Filename);
                 this.PdfContent = content;
             }
 
-            public void SetFoldername()
+            public void SetFoldername(string value)
             {
-                this.Foldername = @"C:\Users\rennd\OneDrive\Dokumente\Bücher\StrukturDynamik";
+                if (!String.IsNullOrEmpty(value))
+                    this.Foldername = value; /// @"C:\Users\rennd\OneDrive\Dokumente\Bücher\StrukturDynamik";
+                else
+                    Console.WriteLine("Enter a foldername");
             }
 
-            public void SetFilename()
+            public void SetFilename(string value)
             {
-                this.Filename = "zahlen"; //"2012_Book_FEM.pdf";
+                if(!String.IsNullOrEmpty(value))
+                    this.Filename = value; ///"2012_Book_FEM.pdf";
+                else
+                    Console.WriteLine("Enter a filename");
             }
+            public int AllNumbers { get; private set; }
+
 
             public bool CheckFolder() ///(string foldername)
             {
                 this.Foldername = @"C:\Users\rennd\OneDrive\Dokumente\Bücher\StrukturDynamik\";
+
+                if(String.IsNullOrEmpty(Foldername))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("No folder path available");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+
+                    Environment.Exit(1);
+                }
 
                 if (System.IO.Directory.Exists(Foldername)) ///(foldername))
                 {
@@ -57,7 +73,15 @@ namespace BenfordSet
 
             public bool CheckFile() ///(string filename)
             {
-                this.Filename = "zahlen.txt"; ///"2012_Book_FEM.pdf";
+                this.Filename = "2012_Book_FEM.pdf";
+                if (String.IsNullOrEmpty(Filename))
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("No filename available");
+                    Console.ForegroundColor = ConsoleColor.Gray;
+                    Environment.Exit(1);
+                }
+
                 if (System.IO.File.Exists(Foldername + Filename))
                 {
                     Console.ForegroundColor = ConsoleColor.Green;
@@ -72,62 +96,128 @@ namespace BenfordSet
                     Console.ForegroundColor = ConsoleColor.Gray;
                     Environment.Exit(1);
                     return false;
-                }
+                }     
             }
 
-            public void FindNumbers()
-            {
+            //public void GetPdfContent()
+            //{
+            //    using (PdfDocument document = PdfDocument.Open(@"C:\Users\rennd\OneDrive\Dokumente\Bücher\StrukturDynamik\2012_Book_FEM.pdf"))
+            //    {
+            //        foreach (Page page in document.GetPages())
+            //        {
+            //            IReadOnlyList<Letter> letters = page.Letters;
+            //            string example = string.Join(string.Empty, letters.Select(x => x.Value));
+            //            //Console.WriteLine(example);
+            //            IEnumerable<Word> words = page.GetWords();
 
+            //            IEnumerable<IPdfImage> images = page.GetImages();
+            //            //Console.WriteLine(images);
+            //            //PdfContent = example;
+            //        }
+            //    }
+            //}
+
+            public void CountNumbers(string raw)
+            {
+                int[] numbers = new int[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                int AllNumbers = 0;
 
                 Regex regex = new Regex(@"[0-9]*[0-9]");
 
-                foreach (Match match in regex.Matches(PdfContent))
+                foreach (Match match in regex.Matches(raw))
                 {
-                    Console.WriteLine(match.Value.StartsWith("1"));
-                    Console.WriteLine(match.Value.StartsWith("4"));
+                    AllNumbers += 1; 
+                    if (match.Value.StartsWith("1"))
+                        numbers[0] += 1;
 
-                    Console.WriteLine("Found '{0}' at position {1}", match.Value, match.Index);
+                    else if (match.Value.StartsWith("2"))
+                        numbers[1] += 1;
+
+                    else if (match.Value.StartsWith("3"))
+                        numbers[2] += 1;
+
+                    else if (match.Value.StartsWith("4"))
+                        numbers[3] += 1;
+
+                    else if (match.Value.StartsWith("5"))
+                        numbers[4] += 1;
+
+                    else if (match.Value.StartsWith("6"))
+                        numbers[5] += 1;
+
+                    else if (match.Value.StartsWith("7"))
+                        numbers[6] += 1;
+
+                    else if (match.Value.StartsWith("8"))
+                        numbers[7] += 1;
+
+                    else if (match.Value.StartsWith("9"))
+                        numbers[8] += 1;
+
+                    else
+                        Console.WriteLine("I dont know what to do....");
                 }
 
-                //for (int i = 0; i <= PdfContent.Le ngth; i++)
-                //{
-                //if (regex.IsMatch(PdfContent))
-                //{
-                //        Console.Write("in if");
-                //        if (PdfContent[i].ToString() == "1")
-                //        {
-                //            Console.WriteLine("1 wurde gefunden zähle 1 hoch");
-                //        }
-                //        else if (PdfContent[i].ToString() == "4")
-                //        {
-                //            Console.WriteLine("4 wurde gefunden zähle 1 hoch");
-                //        }
-                //    }
-
-                //}
-
-
-                ///Console.WriteLine(Regex.Matches(PdfContent, pattern));
-            }
-
-            public void CountNumbers()
-            {
-
-            }
-
-
-            class AnalysePdf : GetPdf
-            {
-                public int Count { get; set; }
-
-                public int Distribution { get; set; }
-
-                public void CountNumbers(string content)
+                for (int i = 1; i <= 9; i++)
                 {
+                    Console.WriteLine("Anzahl der Zahl {0}: {1}", i, numbers[i - 1]);
+                }
+                Console.WriteLine("All numbers in the file: {0}", AllNumbers);
+                CalculateDistribution(numbers, AllNumbers);
+            }
 
+            public void CalculateDistribution(int[] numbers, int AllNumbers)
+            {
+                double[] digits = new double[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                double[] convertNumbers = new double[9] { 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+                Console.WriteLine(AllNumbers);
+
+                for(int z=0; z <= 8 ; z++)
+                {
+                    convertNumbers[z] = (double)numbers[z];
                 }
 
+                if(AllNumbers != 0)
+                {
+                    for (int k = 0; k <= 8; k++)
+                    {
+                        digits[k] = Math.Round(convertNumbers[k] / AllNumbers * 100, 1);
+                    }
+                }        
+                else
+                {
+                    Console.WriteLine("There are no numbers in the file");
+                    Environment.Exit(1);
+                }
+
+                foreach (int digit in digits)
+                {
+                    Console.WriteLine(digit + " %");
+                }
+                BenfordNumbers(digits);
             }
+
+
+            public void BenfordNumbers(double[] digits)
+            {
+                double[] benfordNumbers = new double[9] { 30.1, 17.6, 12.5, 9.7, 7.9, 6.7, 5.8, 5.1, 4.6 };
+
+                //foreach (double num in benfordNumbers)
+                //    Console.WriteLine(num + "\t%");
+                PrinResults(benfordNumbers, digits);
+            }
+
+           
+
+            public void PrinResults(double[] benford, double[] digits)
+            {
+                Console.WriteLine("Benford Distribution \t Your Distribution \t Difference ");
+                for (int i = 0; i <= benford.Length - 1; i++)
+                {
+                    Console.WriteLine("{0}: {1} % \t\t {2}: {3} %  \t\t {4}: {5} %",i+1, benford[i], i+1, digits[i], i+1,i);
+                }
+            }
+
 
             static void Main(string[] args)
             {
@@ -135,13 +225,34 @@ namespace BenfordSet
                 //string fld = @"C:\Users\rennd\OneDrive\Dokumente\Bücher\StrukturDynamik\";
                 //string fil = "zahlen.txt";
                 GetPdf pdf = new GetPdf();
-                pdf.SetFoldername();
-                pdf.SetFilename();
+                //Console.WriteLine("Enter folder path and file name");
+                //string foldername = Console.ReadLine();
+                //string filename = Console.ReadLine();
+                //pdf.SetFoldername(foldername);
+                //pdf.SetFilename(filename);
                 pdf.CheckFolder();
                 pdf.CheckFile();
-                pdf.ReadPdf();
-
-                pdf.FindNumbers();
+                ///pdf.ReadPdf();
+                ///                
+                using (PdfDocument document = PdfDocument.Open(@"C:\Users\rennd\OneDrive\Dokumente\Bücher\StrukturDynamik\2012_Book_FEM.pdf"))
+                {
+                    //string rawText = "";
+                    foreach (Page page in document.GetPages())
+                    {
+                        IReadOnlyList<Letter> letters = page.Letters;
+                        string example = string.Join(string.Empty, letters.Select(x => x.Value));
+                        IEnumerable<Word> words = page.GetWords();
+                        string rawText = "";
+                        rawText += page.Text;
+                        pdf.PdfContent += rawText;
+                        IEnumerable<IPdfImage> images = page.GetImages();
+                        pdf.PdfContent += rawText;
+                    }
+                   
+                    
+                    pdf.CountNumbers(pdf.PdfContent);
+                    ///pdf.BenfordNumbers();
+                }
 
             }
         }
