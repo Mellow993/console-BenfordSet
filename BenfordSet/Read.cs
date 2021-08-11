d8f2c636bda3932d45247359c9da4d217e62420a
Oliver <o.panzer@yahoo.com>
2021-08-11 13:22:03 +0200

    Adding class structure

--- BenfordSet/Read.cs
+++ BenfordSet/Read.cs
@@ -9,34 +9,44 @@ namespace BenfordSet
     abstract class Read
     {
         public string Source { get; set; }
-        public int GetPageNumber { get; set; }
-        public string Content { get; set; }
+        public int PageNumber { get; protected set; }
+        public int ImageNumber { get; protected set; }
+        public string Information { get; protected set; }
+        public string Content { get; protected set; }
 
         abstract public void GetContent();
 
     }
     class Readpdf : Read
     {
-        public Readpdf(string content)
+        public Readpdf(string source)
         {
-            this.Content = content;
+            this.Source = source;
         }
         override public void GetContent()
         {
-            using (PdfDocument document = PdfDocument.Open(Content)) ///@"C:\Users\rennd\OneDrive\Dokumente\Bücher\StrukturDynamik\2012_Book_FEM.pdf"))
+            try
             {
-
-                foreach (Page page in document.GetPages())
+                using (PdfDocument document = PdfDocument.Open(Source))
                 {
-                    //IReadOnlyList<Letter> letters = page.Letters;
-                    //string example = string.Join(string.Empty, letters.Select(x => x.Value));
-                    //IEnumerable<Word> words = page.GetWords();
-                    Content += page.Text;
-                    //IEnumerable<IPdfImage> images = page.GetImages();
-                    GetPageNumber = page.Number;
+                    foreach (Page page in document.GetPages())
+                    {
+                        Content += page.Text;
+                        PageNumber = page.Number;
+                        ImageNumber = page.NumberOfImages;
+                        //IReadOnlyList<Letter> letters = page.Letters;
+                        //string example = string.Join(string.Empty, letters.Select(x => x.Value));
+                        //IEnumerable<Word> words = page.GetWords();
+                        //IEnumerable<IPdfImage> images = page.GetImages();
+                    }
+                    Console.WriteLine(Content);
                 }
-                Console.WriteLine(Content);
             }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 
@@ -49,6 +59,14 @@ namespace BenfordSet
     }
 
     class Readwordfile : Read
+    {
+        public override void GetContent()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    class Readwebpage : Read
     {
         public override void GetContent()
         {
